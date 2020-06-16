17d66bc30c5fbeb443edccfc57d1926c7dcea131
AntoineZappa <antgonbla@gmail.com>
2020-06-16 19:05:49 +0200

    2nd ver. Adding multiprocessing but incompleted

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import multiprocessing
 import time
 
 start = time.perf_counter()
@@ -9,8 +10,11 @@ def do_something():
     time.sleep(1)
     print('Done Sleeping...')
 
-do_something()
-do_something()
+p1 = multiprocessing.Process(target=do_something)
+p2 = multiprocessing.Process(target=do_something)
+
+p1.start()
+p2.start()
 
 finish = time.perf_counter()
 
