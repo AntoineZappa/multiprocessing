94e19a00869448e9127440dacf0730d49171e88d
AntoineZappa <antgonbla@gmail.com>
2020-06-17 10:44:32 +0200

    Adding variable seconds and args to the multiprocessing

--- start.py
+++ start.py
@@ -5,18 +5,20 @@ import time
 
 start = time.perf_counter()
 
-#that is a sismple function to sleep 1 second = time.sleep(1)
-def do_something():
+# passing a variable throw the function
+def do_something(seconds):
     """"""
-    print('Sleeping 1 second...')
-    time.sleep(1)
+    print(f'Sleeping {seconds} second...')
+    #that is a sismple function to sleep 1 second = time.sleep(1)
+    time.sleep(seconds)
     print('Done Sleeping...')
 # A list to store the joining of
 processes = []
 
 # This is a loop start the processes
 for _ in range(10): #underscore indicates a throwaway variable name
-    p = multiprocessing.Process(target=do_something)
+    # Because the variable seconds it's added, we need an arg to the multiprocessing
+    p = multiprocessing.Process(target=do_something, args=[1.5])
     p.start()
     processes.append(p)
 #loop to join
