74c6d14b1afa003f8281063555f11e9bde2c2d98
AntoineZappa <antgonbla@gmail.com>
2020-06-19 10:17:46 +0200

    Changing the method using concurrent.futures module.

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import multiprocessing
+import concurrent.futures  #different way to do multiprocessing
 import time
 
 start = time.perf_counter()
@@ -8,22 +8,30 @@ start = time.perf_counter()
 # passing a variable throw the function
 def do_something(seconds):
     """"""
-    print(f'Sleeping {seconds} second...')
+    print(f'Sleeping {seconds} second(s)...')
     #that is a sismple function to sleep 1 second = time.sleep(1)
     time.sleep(seconds)
-    print('Done Sleeping...')
+    return 'Done Sleeping...'
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    f1 = executor.submit(do_something, 1) #submit schedule a function to a future object
+    print(f1.result())
+
+
 # A list to store the joining of
 processes = []
 
-# This is a loop start the processes
-for _ in range(10): #underscore indicates a throwaway variable name
-    # Because the variable seconds it's added, we need an arg to the multiprocessing
-    p = multiprocessing.Process(target=do_something, args=[1.5])
-    p.start()
-    processes.append(p)
-#loop to join
-for process in processes:
-    process.join()
+
+# # This is a loop to start the processes
+# for _ in range(10): #underscore indicates a throwaway variable name
+#     # Because the variable seconds it's added, we need an arg to the multiprocessing
+#     p = multiprocessing.Process(target=do_something, args=[1.5])
+#     p.start()
+#     processes.append(p)
+# #loop to join
+# for process in processes:
+#     process.join()
 
 finish = time.perf_counter()
 
